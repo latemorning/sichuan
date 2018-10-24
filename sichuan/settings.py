d787ee73a234ca7ddbbcfea7df7970c490cbb36e
harry <latemorning@gmail.com>
2018-10-24 10:04:43 +0900

    mariadb 셋팅

--- sichuan/settings.py
+++ sichuan/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = 'sichuan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sichuan',
+        'USER': 'sichuan',
+        'PASSWORD': 'sichuan',
+        'HOST': '13.125.23.94',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
