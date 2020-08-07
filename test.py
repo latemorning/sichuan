29805179a550d39864835f2ec3cad52d2ea6a5e2
harry <91228852@ktfriend.com>
2020-08-07 17:41:09 +0900

    test commit

--- test.py
+++ test.py
@@ -3,3 +3,5 @@ def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
     return [b"Hello World"] # python3
     #return ["Hello World"] # python2
+    #return ["Hello World"] # python2
+    #return ["Hello World"] # python2
